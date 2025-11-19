7e29f77a90a70832838e9a8abb316d2781b4e177
Varsha Raikwar <varsha.raikwar@zendesk.com>
2025-11-19 23:38:15 +0530

    create signout functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def logout
+    sign_out(current_user)
+    redirect_to root_path, notice: "Signed out successfully."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   devise_for :users
+   get "/logout", to: "home#logout", as: :custom_logout
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
